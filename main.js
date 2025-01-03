13c098f437cc92e662476e98a6934d2c03e1f0f4
Savan Gondaliya <savangondaliya0@gmail.com>
2025-01-03 09:42:56 +0530

    new Announcement

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ function Time()
     let deg = 6;
     const date = new Date();
     let hour  = date.getHours()*30;
-    let minute  = date.getMinutes()*deg;
-    let second  = date.getSeconds()*deg;
+    let minute  = date.getMinutes() * deg;
+    let second  = date.getSeconds() * deg;
     document.getElementById('hh').style.transform = `rotateZ(${hour+(minute/12)}deg)`;
     document.getElementById('mm').style.transform = `rotate(${minute}deg)`;
     document.getElementById('ss').style.transform = `rotate(${second}deg)`;
@@ -22,3 +22,19 @@ function light_mode() {
     document.getElementById("darkMode").innerHTML = darkMode?"Dark Mode":"Light Mode";
     darkMode=!darkMode;
 }
+
+const clockContainer = document.querySelector(".clock_container");
+
+document.addEventListener("DOMContentLoaded",() => {
+
+    const messageContainer = document.createElement("div");
+    messageContainer.classList.add("messageContainer");
+    clockContainer.appendChild(messageContainer);
+    document.body.style.filter = blur("10px");
+
+    messageContainer.innerHTML =`
+        <div>
+            <h3>We Are Coming With Something New in Few Days</h3>        
+        </div>
+    `
+})
